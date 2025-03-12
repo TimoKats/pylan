ff69fc4ac4f8549e935c62d583c4ccd5e489c3fc
TimoKats <tpakats@gmail.com>
2025-03-12 20:12:47 +0100

    also import result class

--- pylan/__init__.py
+++ pylan/__init__.py
@@ -3,4 +3,5 @@ from pylan.item import Item  # noqa: F401
 from pylan.patterns.add import Add  # noqa: F401
 from pylan.patterns.divide import Divide  # noqa: F401
 from pylan.patterns.multiply import Multiply  # noqa: F401
+from pylan.patterns.result import Result  # noqa: F401
 from pylan.patterns.subtract import Subtract  # noqa: F401
