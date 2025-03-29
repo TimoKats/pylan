b8a5358a3028d21f26a8b3c70512e7d970a7785b
TimoKats <tpakats@gmail.com>
2025-03-29 18:17:10 +0100

    fix smoke tests?

--- misc/smoke_test.py
+++ misc/smoke_test.py
@@ -1,14 +1,18 @@
-from pylan import AddGrow, Divide, Item, Subtract
+from pylan import Add, Granularity, Item, Multiply, Result, Subtract
 
-savings = Item(start_value=100)
-dividends = AddGrow("90d", 100, "1y", 1.1)
-growing_salary = AddGrow("1m", 2500, "1y", 1.2, offset="24d")
+savings = Item(start_value=10)
+salary_payments = Add("1m", 2500, offset="24d")
+salary_increase = Multiply("1y", 1.2)
 mortgage = Subtract("0 0 2 * *", 1500)  # cron support
-inflation = Divide(["2025-1-1", "2026-1-1", "2027-1-1"], 1.08)
+salary_payments.add_pattern(salary_increase)
+savings.add_patterns([salary_payments])
 
-savings.add_patterns([growing_salary, dividends, mortgage])
-result = savings.run("2024-1-1", "2027-1-1")
+result = Result()
+car_bought = False
+buy_car = Subtract("", 5000)
 
-x, y = result.plot_axes()
-
-print(result["2024-5-5"])
+for date, saved in savings.iterate("2024-1-1", "2025-1-1", Granularity.day):
+    if saved.value > 5000 and not car_bought:
+        buy_car.apply(savings)
+        car_bought = True
+    result.add_result(date=date, value=saved.value)
